145e2b2d82be411f1e7971f205ed9d8800997809
YeonDeok <yeondeok99@naver.com>
2024-07-30 17:43:47 +0000

    Pending changes exported from your codespace

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -1,9 +1,17 @@
 #include "Zombie.hpp"
+#include <stdlib.h>
+
 
 Zombie* zombieHorde(int N, std::string name);
 
+void    f(void)
+{
+    system("leaks horde");
+}
+
 int main(void)
 {
+    atexit(f);
     Zombie* horde = zombieHorde(10, "zzzzzombieeeeeeeee");
     for (int i = 0; i < 10; ++i)
         horde[i].announce();
--- cpp04/ex03/MateriaSource.cpp
+++ cpp04/ex03/MateriaSource.cpp
@@ -1,2 +1,31 @@
 #include "MateriaSource.hpp"
 
+MateriaSource::MateriaSource()
+{
+	
+}
+
+MateriaSource::MateriaSource(const MateriaSource& ref)
+{
+
+}
+
+MateriaSource::~MateriaSource()
+{
+
+}
+
+MateriaSource&	MateriaSource::operator=(const MateriaSource& ref)
+{
+
+}
+
+void	MateriaSource::learnMateria(AMateria* materia)
+{
+
+}
+
+AMateria*	createMateria(std::string const & type)
+{
+
+}
